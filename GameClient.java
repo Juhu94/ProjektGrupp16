61ea20cdbecbf23c6e837181e65007928638aaa4
Juhu94 <julian.hultgren@hotmail.com>
2017-04-10 11:58:21 +0200

    Update GameClient.java

--- GameClient.java
+++ GameClient.java
@@ -5,10 +5,12 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 
-import BoardGame.Viewer;
+
+
 /**
  * 
  * @author Julian Hultgren
@@ -16,10 +18,18 @@ import BoardGame.Viewer;
  */
 public class GameClient {
 	private Socket socket;
-	private Viewer viewer;
+	private ArrayList<ViewerListener> listeners = new ArrayList<ViewerListener>();
 	
-	public GameClient(String serverIp, int port){
-		new Connection(serverIp,port).start();
+//	public GameClient(String serverIp, int port){
+//		new Connection(serverIp,port).start();
+//	}
+	
+	public GameClient(){
+		
+	}
+	
+	public void addListeners(ViewerListener listener) {
+		listeners.add(listener);
 	}
 	
 	private class Connection extends Thread {
@@ -32,35 +42,39 @@ public class GameClient {
 			this.port = port;
 		}
 		
-		public void run(){
-			System.out.println("Client Running");
-			try{
-				socket = new Socket(ipAddress,port);
-				
-			}catch (IOException e ){
-				e.printStackTrace();
-			}
-			
-			while(!this.currentThread().isInterrupted()){
-				try{
-					
-					ObjectInputStream oInput = new ObjectInputStream(socket.getInputStream());
-					DataInputStream dInput = new DataInputStream(socket.getInputStream());
-					
-					
-				}catch (IOException e){
-					e.printStackTrace();
-				}
-			}
-		}
+//		public void run(){
+//			System.out.println("Client Running");
+//			try{
+//				socket = new Socket(ipAddress,port);
+//				
+//			}catch (IOException e ){
+//				e.printStackTrace();
+//			}
+//			
+//			while(!this.currentThread().isInterrupted()){
+//				try{
+//					
+//					ObjectInputStream oInput = new ObjectInputStream(socket.getInputStream());
+//					DataInputStream dInput = new DataInputStream(socket.getInputStream());
+//					
+//					
+//				}catch (IOException e){
+//					e.printStackTrace();
+//				}
+//			}
+//		}
 	}
 	
 	public void changeTileColor(JLabel theLabel){
-		theLabel.setBackground(Color.RED);
+		for(ViewerListener listener : listeners){
+			listener.updateViewer(theLabel);
+		}
+		
 	}
 	
 	public static void main(String[] args) {
-		GameClient cc = new GameClient("127.0.0.1" ,3520);
-		Viewer vv = new Viewer();
+//		GameClient cc = new GameClient("127.0.0.1" ,3520);
+		GameClient cc = new GameClient();
+		Viewer vv = new Viewer(cc);
 	}
 }
