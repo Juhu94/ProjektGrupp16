15c47cb01c6871e0c81c0ff08e06a1afc2fc90b8
LukPer1 <lukas.persson1@hotmail.com>
2017-04-12 11:42:03 +0200

    Update SpecialTile.java

--- SpecialTile.java
+++ SpecialTile.java
@@ -1,15 +1,15 @@
 package sjörövarön;
 
 public class SpecialTile extends Tile{
-	private Tile sucsess;
+	private Tile success;
 	private Tile fail;
 
 	public SpecialTile(){
 		super("Special", true, true, false);
 	}
 	
-	public Tile sucsess(){
-		return sucsess;
+	public Tile success(){
+		return success;
 	}
 	
 	public Tile fail(){
