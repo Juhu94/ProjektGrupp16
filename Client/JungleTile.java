3e91a3e7b49d4ff4c7b2d3d93954217ab78ca4e1
Juhu94 <julian.hultgren@hotmail.com>
2017-04-28 14:57:57 +0200

    Add files via upload

---
+++ Client/JungleTile.java
@@ -0,0 +1,17 @@
+package Client;
+/**
+ * 
+ * @author 
+ * Version 1.0
+ */
+public class JungleTile extends Tile{
+	
+	/**
+	 *Constructor
+	 */
+	
+	public JungleTile(){
+		super("Jungle", false, false, false);
+	}
+
+}
---
+++ Gui/IconPanel.java
@@ -0,0 +1,27 @@
+package Gui;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+/**
+ * 
+ * @author Julian Hultgren
+ * Version 1.0
+ *
+ */
+public class IconPanel extends JPanel {
+	private ImageIcon icon;
+	
+	public IconPanel(ImageIcon icon) {
+		this.icon = icon;
+	}
+	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Image im = icon.getImage();
+		g.drawImage(im, 0, 0, getWidth(), getHeight(), null);
+	}
+}
+ 
