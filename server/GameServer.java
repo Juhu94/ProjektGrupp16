b4f86c7dbfed34f32aad2ece4a341301a1f9d35d
SimonBorjesson <simon.b.93@hotmail.com>
2017-05-03 22:58:40 +0200

    metod createCharacter färdig

--- server/GameServer.java
+++ server/GameServer.java
@@ -8,9 +8,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.text.AbstractDocument.BranchElement;
 
 import client.Tile;
 /**
@@ -24,7 +26,8 @@ public class GameServer implements Runnable{
 	private Thread serverThread = new Thread(this);
 	private HashMap<String, ClientHandler> clientMap = new HashMap<String, ClientHandler>();
 	private HashMap<Integer, String> clientMapid = new HashMap<Integer, String>();
-	private HashMap<String, Character> characterMap = new HashMap<String, Character>();
+	private HashMap<String, client.Character> characterMap = new HashMap<String, client.Character>();
+	private Random rad = new Random();
 	
 	private int counter = 1;
 	private int id = 1;
@@ -62,6 +65,7 @@ public class GameServer implements Runnable{
 		ObjectOutputStream output;
 		private String username;
 		private int nbrOfPlayers;
+		private int playerid;
 		
 		
 		public ClientHandler(Socket socket) {
@@ -82,6 +86,7 @@ public class GameServer implements Runnable{
 						username = (String)object;
 						clientMap.put(username, this);
 						clientMapid.put(id, username);
+						playerid = id;
 						id++;
 					}
 					if(object instanceof Boolean){
@@ -90,6 +95,9 @@ public class GameServer implements Runnable{
 					}
 					
 				}catch (IOException | ClassNotFoundException e) {
+//					characterMap.remove(username);
+//					clientMapid.remove(playerid);
+//					clientMap.remove(username);
 					closeSocket();
 					e.printStackTrace();
 				}
@@ -128,10 +136,71 @@ public class GameServer implements Runnable{
 			}
 		}
 		
-		public void createCharacter(){
-			//in progress
+		/**
+		 * Creates a character and places it on a empty starting position
+		 */
+
+		public void createCharacter() {
+			client.Character myCharacter = new client.Character(username, -1, -1);
+			while (myCharacter.getRow() == -1) {
+				int startPos = rad.nextInt(5);
+				switch (startPos) {
+				case 0:
+					if (!checkCharacterAtPos(9, 4)) {
+						myCharacter.setPos(9, 4);
+					}
+					break;
+				case 1:
+					if (!checkCharacterAtPos(6, 22)) {
+						myCharacter.setPos(6, 22);
+					}
+					break;
+				case 2:
+					if (!checkCharacterAtPos(9, 35)){
+						myCharacter.setPos(9, 35);
+						}
+					break;
+				case 3:
+					if(!checkCharacterAtPos(23, 4)){
+						myCharacter.setPos(23, 4);
+						}
+					break;
+				case 4:
+					if(!checkCharacterAtPos(31, 44)){
+						myCharacter.setPos(31, 44);
+						}
+					break;
+				case 5:
+					if(!checkCharacterAtPos(24, 37)){
+						myCharacter.setPos(24, 37);
+						}
+					break;
+				}
+			}
+			characterMap.put(username, myCharacter);
 		}
 		
+		/**
+		 * Checks if there is a character at the given position, returns true if
+		 * there is and false if there is not
+		 * @param row	int
+		 * @param col	int
+		 * @return 		boolean
+		 */
+
+		public boolean checkCharacterAtPos(int row, int col) {
+			String userID;
+			for (int i = 1; i == id; i++) {
+				if (clientMap.containsKey(i)) {
+					userID = clientMapid.get(i);
+					if (characterMap.get(userID).getRow() == row && characterMap.get(userID).getCol() == col) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
 		public void closeSocket() {
 			try {
 				socket.close();
