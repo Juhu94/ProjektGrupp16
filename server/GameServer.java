710083567afe0a345399645428b055dd3bcf9b0f
LukPer1 <lukas.persson1@hotmail.com>
2017-05-03 17:49:54 +0200

    V0.7 GameServer

--- server/GameServer.java
+++ server/GameServer.java
@@ -10,19 +10,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import client.Tile;
 /**
  * 
- * @author Julian Hultgren
- * Version 0.6
+ * @author Julian Hultgren, Lukas Persson
+ * Version 0.7
  */
 public class GameServer implements Runnable{
 
 	private ServerSocket serverSocket;
 	private Thread serverThread = new Thread(this);
 	private HashMap<String, ClientHandler> clientMap = new HashMap<String, ClientHandler>();
+	private HashMap<Integer, String> clientMapid = new HashMap<Integer, String>();
 	private HashMap<String, Character> characterMap = new HashMap<String, Character>();
+	
+	private int counter = 1;
+	private int id = 1;
 	
 	public GameServer(int port){
 		try{
@@ -56,6 +61,9 @@ public class GameServer implements Runnable{
 		ObjectInputStream input;
 		ObjectOutputStream output;
 		private String username;
+		private int nbrOfPlayers;
+		
+		
 		public ClientHandler(Socket socket) {
 			this.socket = socket;
 			try {
@@ -73,6 +81,12 @@ public class GameServer implements Runnable{
 					if(object instanceof String){
 						username = (String)object;
 						clientMap.put(username, this);
+						clientMapid.put(id, username);
+						id++;
+					}
+					if(object instanceof Boolean){
+						boolean enableButtons = (boolean)object;
+						clientsTurn(enableButtons);
 					}
 					
 				}catch (IOException | ClassNotFoundException e) {
@@ -81,6 +95,39 @@ public class GameServer implements Runnable{
 				}
 			}
 		}
+		/**
+		 * Method to enable the buttons for the next player
+		 * goes through the number of players and when it reaches
+		 * the number of players it goes back down to 1
+		 * @param enableButtons boolean
+		 */
+		public void clientsTurn(boolean enableButtons){
+			nbrOfPlayers = clientMap.size();
+			if(counter == nbrOfPlayers){
+				String username = clientMapid.get(counter);
+				ClientHandler ch = clientMap.get(username);
+				try {
+					ch.output.writeObject(enableButtons);
+					ch.output.flush();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				counter = 1;
+			}
+			else{
+				String username = clientMapid.get(counter);
+				ClientHandler ch = clientMap.get(username);
+				try{
+					ch.output.writeObject(enableButtons);
+					ch.output.flush();
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+				counter++;
+			}
+		}
+		
 		public void createCharacter(){
 			//in progress
 		}
