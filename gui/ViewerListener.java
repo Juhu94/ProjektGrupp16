42435d0382d7b8f8c86ae1fcac90f5698e44cd95
Juhu94 <julian.hultgren@hotmail.com>
2017-05-04 09:17:52 +0200

    Add files via upload

--- gui/ViewerListener.java
+++ gui/ViewerListener.java
@@ -4,6 +4,7 @@ import javax.swing.Icon;
 import javax.swing.JLabel;
 
 public interface ViewerListener {
-	public void updateViewer(JLabel woopiedo);
+	public void updateViewer(ExtendedJLabel theLabel);
 	public void updateViewer(boolean enableButtons);
+	public void paintCharacter(int row, int col);
 }
